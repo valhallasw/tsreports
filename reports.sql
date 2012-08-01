9a907d3fb291e5de29f20d0404cbdf7d4278011a
Merlijn van Deen <valhallasw@gmail.com>
2012-08-01 19:26:38 +0000

    Added column to store then the last query update was started.

--- reports.sql
+++ reports.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS report_cache;
 CREATE TABLE report_cache (
 	dbname varchar(64) NOT NULL default '',
 	report_key varchar(64) NOT NULL default '',
 	last_run int(11) default NULL,
+	last_start int(11) default NULL,
 	result longblob,
 	PRIMARY KEY  (`dbname`,`report_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
