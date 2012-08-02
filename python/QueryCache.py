4191d6e15f4d25d25042f3cb72215ab3e9311a0f
Merlijn van Deen <valhallasw@gmail.com>
2012-08-02 19:13:44 +0000

    Remove cache status return value from run_background_update

--- python/QueryCache.py
+++ python/QueryCache.py
@@ -85,8 +85,8 @@ class QueryCache:
                 if age < report.cache:
                     return {'status': 'hot', 'age': age, 'result': result}
                 else:
-                    status, query_runtime = self.run_background_update(dbname, report, variables)
-                    return {'status': status, 'query runtime': query_runtime, 
+                    query_runtime = self.run_background_update(dbname, report, variables)
+                    return {'status': 'cold', 'query runtime': query_runtime, 
                             'age': age, 'result': result}
 
             # Not cached; if it's a nightly query, return failure
@@ -112,9 +112,9 @@ class QueryCache:
         if time_since_last_start is None or \
            time_since_last_start > report.cache:
             threading.Thread(target=regenerator).start()
-            return ('cold', 0)
+            return 0
         else:
-            return ('cold', time_since_last_start)
+            return time_since_last_start
 
 
     def update_report(self, dbname, report, variables):
