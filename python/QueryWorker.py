dda0419ea95752f4a77fc66fe78ee4dc67886830
Merlijn van Deen <valhallasw@gmail.com>
2012-08-05 16:10:03 +0000

    Added logging to QueryWorker

--- python/QueryWorker.py
+++ python/QueryWorker.py
@@ -7,9 +7,13 @@
 # warranty.
 #
 
-def update_report(wiki, report, parameters):
+def get_homedir():
     import pwd, posix
     homedir = pwd.getpwuid(posix.getuid())[5]
+    return homedir    
+
+def get_context():
+    homedir = get_homedir()
     cfgfile = homedir + "/.reports.cfg"
     cfg = dict()
     execfile(cfgfile, cfg)
@@ -18,9 +22,25 @@ def update_report(wiki, report, parameters):
     import sys
     sys.path.append(cfg['base'] + "/python")
 
-    from Reports import ReportContext, Report
-    from QueryCache import *
+    from Reports import ReportContext
+    return ReportContext(cfg)
+        
 
-    context = ReportContext(cfg)
+def update_report(wiki, report, parameters):
+    from QueryCache import QueryCache
+    context = get_context()
+
     cache = QueryCache(context)
     cache.update_report(wiki, report, parameters)
+
+def logging_update_report(wiki, report, parameters):
+    try:
+        update_report(wiki, report, parameters)
+    except Exception, e:
+        import os, traceback, time
+        f = open(os.path.join(get_homedir(), 'reports.err'), 'a')
+        f.write('='*80 + '\n')
+        f.write("At %s, \n" % time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()))
+        f.write("While executing update_report(%r, %r, %r):\n\n" % (wiki, report, parameters))
+        traceback.print_exc(file=f)
+        f.close()
