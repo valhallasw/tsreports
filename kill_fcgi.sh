74b4e5a2653ca0744f3575b2c0b16410bc7e7526
Merlijn van Deen <valhallasw+tools@gmail.com>
2014-11-15 22:14:35 +0000

    Fix kill_fcgi

--- kill_fcgi.sh
+++ kill_fcgi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-PROJECTNAME=`echo $USER | sed -e 's/local-//'`
-ps aux | grep $PROJECTNAME | grep python | grep fcgi | awk '{print $2}' | xargs kill 2>/dev/null
+PROJECTNAME=`echo $USER | sed -e 's/tools.//'`
+ps aux | grep $PROJECTNAME | grep python | grep fcgi | awk '{print $2}' | xargs kill
